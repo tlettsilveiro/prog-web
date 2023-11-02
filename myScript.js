2ffe3d483c90e6f93c78ded9a4e9dd6fba2615d1
tlettsilveiro <tomylett@gmail.com>
2023-11-02 07:36:33 -0300

    *

---
+++ myScript.js
@@ -0,0 +1,20 @@
+// Script para guardar los comentarios
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('miFormulario').addEventListener('submit', function(event) {
+        event.preventDefault(); // Esto previene el comportamiento predeterminado del formulario
+
+        var comentarios = document.getElementById('comentarios').value;
+
+        console.log('Comentarios:', comentarios);
+    });
+});
+
+
+
+
+// Script para que funcionen los botones 'Volver'
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('returnButton').addEventListener('click', function() {
+        window.location.href = 'index.html';
+    });
+});
